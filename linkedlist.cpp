0f0728ad6d83fa81c7467303c1647f6d5efc0b12
Saif Sami <saif.sarker@northsouth.edu>
2026-02-22 11:33:42 +0600

    Added class based Solution

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define endl "\n"
+
 struct node{
 	int data;
 	node* next;
 };
-node* head;
+node* head = NULL;
 
 
 //Adding any value at the back of the linkedlist
@@ -29,7 +31,38 @@ void add(int value){
 	}
 }
 
+void RemoveFirstOccurance(int x){
 
+	if(head == NULL){
+        cout << "List is empty\n";
+        return;
+    }
+
+	node* temp = head;
+
+	if(head->data == x){
+		head = temp->next;
+		delete temp;
+		return;
+		
+	}
+
+	while(temp != NULL){
+		if(temp->next == NULL){
+			cout << "No Values matched with the given value" << endl;
+			return;
+		}
+		if(temp->next->data == x){
+			node* temp2 = temp->next;
+			temp-> next = temp->next->next;
+			delete temp2;
+			return;
+		}
+		
+		temp = temp->next;
+	}
+
+}
 
 void show(){
 	node* iteration = head;
@@ -45,12 +78,16 @@ void show(){
 
 int main(){
 
-	add(29);
-	add(10);
-	add(35);
-	add(1);
+	//add(1);
 	add(5);
+	add(5);
+	//add(7);
+	RemoveFirstOccurance(5);
+	//RemoveFirstOccurance(36);
+	cout << "-------------------"<< endl;
 	show();
+	RemoveFirstOccurance(36);
+
 
 }
 
---
+++ linkedlistWithClassImplementation.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+struct node{
+    int data;
+    node* next;
+};
+
+class LinkedList {
+
+private:
+    node* head = NULL;
+
+public:
+
+    // Adding any value at the back of the linkedlist
+    void add(int value){
+        node* newnode = new node();
+        newnode->data = value;
+        newnode->next = NULL;
+
+        if(head == NULL){
+            head = newnode;
+        }
+        else{
+            node* temp = head;
+            while(temp->next != NULL){
+                temp = temp->next;
+            }
+            temp->next = newnode;
+        }
+    }
+
+    void RemoveFirstOccurance(int x){
+
+        if(head == NULL){
+            cout << "List is empty\n";
+            return;
+        }
+
+        node* temp = head;
+
+        if(head->data == x){
+            head = temp->next;
+            delete temp;
+            return;
+        }
+
+        while(temp != NULL){
+            if(temp->next == NULL){
+                cout << "No Values matched with the given value" << endl;
+                return;
+            }
+            if(temp->next->data == x){
+                node* temp2 = temp->next;
+                temp->next = temp->next->next;
+                delete temp2;
+                return;
+            }
+
+            temp = temp->next;
+        }
+    }
+
+    void show(){
+        node* iteration = head;
+
+        while(iteration != NULL){
+            cout << iteration->data << "\n";
+            iteration = iteration->next;
+        }
+    }
+};
+
+int main(){
+
+    LinkedList list;
+
+    list.add(5);
+    list.add(5);
+
+    list.RemoveFirstOccurance(5);
+
+    cout << "-------------------" << endl;
+    list.show();
+
+    list.RemoveFirstOccurance(36);
+}
